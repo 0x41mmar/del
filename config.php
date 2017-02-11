f5c40ce4d284f8fff8f9a2ae7f1a3699d1d4cffb
Ammar <kazamiken@outlook.com>
2017-02-11 02:41:32 +0000

    callback

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 $CONSUMER_KEY='P2vnqM1dKpqNyq3IsEGk1vqTM';
 $CONSUMER_SECRET='kEmN5mlVruLbz6kpXYNCc1ihOgXH5icGh28hwOovFuQMiMWWDH';
-$OAUTH_CALLBACK='http://www.hbim.org/delegate/login.php';
+$OAUTH_CALLBACK='http://delegate-seiryuu.rhcloud.com/twitteroauth/OAuth.php';
 
 ?>
