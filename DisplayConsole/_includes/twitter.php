659861b988ffddb827d32631df18410d003740b4
Ammar <kazamiken@outlook.com>
2017-02-11 03:25:13 +0000

    display

--- DisplayConsole/_includes/twitter.php
+++ DisplayConsole/_includes/twitter.php
@@ -24,7 +24,12 @@
 	
 	// Set API request URL and timeline variables if needed <https://dev.twitter.com/docs/api/1.1>
 	$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-	$twitterUsername = "DeeTGee";
+	if ( isset($_GET['var']) ){
+		$twitterUsername = $_GET['username'];
+	}
+	else {
+		$twitterUsername = ""
+	}
 	$tweetCount = 200;
 	
 	// Use private tokens for development if they exist; delete when no longer necessary
