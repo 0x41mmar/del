8c0154ce358e95d49481d6aaf2246437d8291f87
Ammar <kazamiken@outlook.com>
2017-02-11 02:50:08 +0000

    callback

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 $CONSUMER_KEY='NwcHug3321eXz8uxhrwLaLSB7';
 $CONSUMER_SECRET='yZ4vPlNrEmgO56Ih1qgYqRDaFlahZMmVA4vQCIjAFjy6JdBVbN';
-$OAUTH_CALLBACK='http://delegate-seiryuu.rhcloud.com/twitteroauth/OAuth.php';
+$OAUTH_CALLBACK='http://delegate-seiryuu.rhcloud.com/oauth.php';
 
 ?>
