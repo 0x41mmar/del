85a37f26e437f9b845fb354307db2c361f634880
Ammar <kazamiken@outlook.com>
2017-02-11 02:30:20 +0000

    first

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$CONSUMER_KEY='P2vnqM1dKpqNyq3IsEGk1vqTM';
+$CONSUMER_SECRET='kEmN5mlVruLbz6kpXYNCc1ihOgXH5icGh28hwOovFuQMiMWWDH';
+$OAUTH_CALLBACK='http://www.hbim.org/delegate/login.php';
+
+?>
---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+echo "Started"
+session_start();
+require_once('twitteroauth/twitteroauth.php');
+include('config.php');
+
+if(isset($_SESSION['name']) && isset($_SESSION['twitter_id'])) //check whether user already logged in with twitter
+{
+	// echo "Name :".$_SESSION['name']."<br>";
+	// echo "Twitter ID :".$_SESSION['twitter_id']."<br>";
+	// echo "Image :<img src='".$_SESSION['image']."'/><br>";
+	// echo "<br/><a href='logout.php'>Logout</a>";
+	echo "<h2>Done! Thanks for your help.</h2>";
+}
+else // Not logged in
+{
+	$connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET);
+	$request_token = $connection->getRequestToken($OAUTH_CALLBACK); //get Request Token
+	if(	$request_token)
+	{
+		$token = $request_token['oauth_token'];
+		$_SESSION['request_token'] = $token ;
+		$_SESSION['request_token_secret'] = $request_token['oauth_token_secret'];
+		switch ($connection->http_code) 
+		{
+			case 200:
+				$url = $connection->getAuthorizeURL($token);
+				//redirect to Twitter .
+		    	header('Location: ' . $url); 
+			    break;
+			default:
+			    echo "Connection to Twitter failed";
+		    	break;
+		}
+	}
+	else //error receiving request token
+	{
+		echo "Error receiving request token";
+	}
+}
+?>
